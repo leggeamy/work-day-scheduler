da4b13cb81189e9438aac1a0e98025c4f0c201f8
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-10-04 17:53:22 -0400

    pseudo code steps

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    //listen for save button clicks
+
+    //get input value
+
+    //save in localStorage
+});
+
+//get current hour
+
+//loop over time blocks 
+
+//check if current time is past the block hour
+
+//set up interval to check if the current time needs to be updated
+
+//load any saved data from localStorage
+
+//display current day on page
